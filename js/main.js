0e6e8e42610435210105369b2b38e698ae1d8fd7
DiviKatieBrown <katie.brown@diviresorts.com>
2024-01-03 10:36:49 -0500

    aruba site v1 complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const myCarouselElement = document.querySelector('#myCarousel');
 const myReviewsCarousel = document.querySelector('#carouselReviews');
+const myGalleryCarousel = document.querySelector('#galleryCarousel')
 
 // Needed for Home Page Gallery to initialize
 const carousel = new bootstrap.Carousel(myCarouselElement, {
@@ -8,6 +9,11 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
 })
 
 const reviewsCarousel = new bootstrap.Carousel(myReviewsCarousel, {
+    interval: 2000,
+    touch: false
+})
+
+const galleryCarousel = new bootstrap.Carousel(myGalleryCarousel, {
     interval: 2000,
     touch: false
 })
